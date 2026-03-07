a7f27e25c80b7ed3a791d65528f905877feff4ca
breezyfc <pscott1097@gmail.com>
2026-03-06 21:00:41 -0500

    .

--- js/ebay.js
+++ js/ebay.js
@@ -1,25 +1,25 @@
-javascript
 const username = "YOUR_EBAY_USERNAME";
 
-const url = `https://corsproxy.io/?https://www.ebay.com/sch/${username}/m.html?_rss=1`;
+const rss = `https://corsproxy.io/?https://www.ebay.com/sch/${username}/m.html?_rss=1`;
 
-fetch(url)
-.then(response => response.text())
-.then(str => new window.DOMParser().parseFromString(str,"text/xml"))
+fetch(rss)
+.then(r => r.text())
+.then(str => new DOMParser().parseFromString(str,"text/xml"))
 .then(data => {
 
 const items = data.querySelectorAll("item");
-const container = document.getElementById("listings");
 
-container.innerHTML="";
+const listings = document.getElementById("listings");
+
+listings.innerHTML="";
 
 items.forEach(el=>{
 
 const title = el.querySelector("title").textContent;
 const link = el.querySelector("link").textContent;
-const img = el.querySelector("enclosure")?.getAttribute("url") || "";
+const img = el.querySelector("enclosure")?.getAttribute("url");
 
-const card = `
+listings.innerHTML += `
 
 <a href="${link}" target="_blank">
 
@@ -38,8 +38,6 @@ const card = `
 </a>
 
 `;
-
-container.innerHTML += card;
 
 });
 
