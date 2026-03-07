0d7d3b64c9105747e85ba3f37056e0869227e93e
breezyfc <pscott1097@gmail.com>
2026-03-06 20:25:03 -0500

    updated everything

---
+++ js/ebay.js
@@ -0,0 +1,52 @@
+const username = "YOUR_EBAY_USERNAME";
+
+const url = `https://api.allorigins.win/raw?url=https://www.ebay.com/sch/${username}/m.html?_ipg=240&rt=nc`;
+
+fetch(url)
+
+.then(res => res.text())
+
+.then(data => {
+
+const parser = new DOMParser();
+
+const doc = parser.parseFromString(data,"text/html");
+
+const items = doc.querySelectorAll(".s-item");
+
+let html="";
+
+items.forEach(item=>{
+
+const title=item.querySelector(".s-item__title")?.innerText;
+const price=item.querySelector(".s-item__price")?.innerText;
+const img=item.querySelector(".s-item__image-img")?.src;
+const link=item.querySelector(".s-item__link")?.href;
+
+if(title && price){
+
+html+=`
+
+<a class="card-item" href="${link}" target="_blank">
+
+<img src="${img}">
+
+<div class="card-info">
+
+<h3>${title}</h3>
+
+<p class="price">${price}</p>
+
+</div>
+
+</a>
+
+`;
+
+}
+
+});
+
+document.getElementById("ebay-listings").innerHTML=html;
+
+});
